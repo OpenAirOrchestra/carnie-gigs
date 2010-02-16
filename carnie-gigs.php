d69a32c5daf89a38e8ec3bb3c8d3e6777e9775f8
Me <me@carnivalband.nfshost.com>
2010-02-16 22:47:52 +0000

    Boilerplate for wordpress..

---
+++ carnie-gigs.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Carnie Gigs
+Plugin URI: http://members.thecarnivalband.com
+Description: A gig calendar plugin for The Carnival Band
+Version: 0.1
+Author: Darryl F
+License: GPL2
+*/
+
+/*  Copyright 2010  Darryl F  (email : darryl.fuller@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USAi
+*/
+?>
