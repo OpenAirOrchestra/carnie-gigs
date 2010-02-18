c44816ccf11be4cd83dcdf80ba6a1cab1c204043
Me <me@carnivalband.nfshost.com>
2010-02-18 18:28:17 +0000

    Initial logic to render short view of gigs in a table.

--- views/gig.php
+++ views/gig.php
@@ -6,8 +6,37 @@ class carnieGigViews {
 
 	/*
 	 * Render short view of gigs from database results
+	 * in an HTML table.
 	 */
 	function shortGigs($gigs) {
+		if (mysql_num_rows($gigs) > 0) {
+
+			print "<table>";
+			while ($gig = mysql_fetch_assoc($result)) {
+				$this->shortGig($gig);
+			}
+			print "</table>";
+		}
+	}
+
+	/*
+	 * Render a single short gig table row
+	 * in an HTML table row
+	 */
+	function shortGig($gig) {
+		print "<tr>";
+		echo "\t<td>" . date('d M Y', strtotime($gig['date'])) . "</td>\n";
+		echo "\t<td>" . stripslashes($gig['title']) . "</td>\n";
+		print "\t<td>";
+		if ($gig['cancelled']) {
+			echo "(cancelled)";
+		}
+		if ($gig['tentative']) {
+			echo "(tentative)";
+		
+		}       
+		print "\t</td>";
+		print "</tr>";
 	}
 }
 ?>
