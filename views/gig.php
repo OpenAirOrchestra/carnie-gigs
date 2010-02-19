838f993b80d33fb7ad1890a524f493137e2e08a0
Me <me@carnivalband.nfshost.com>
2010-02-19 19:49:15 +0000

    Added calendar link to short gig table view.

--- views/gig.php
+++ views/gig.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once('../utility.php');
+
 /*
  * This class handles simple rendering of individual gigs.
  */
@@ -33,6 +36,10 @@ class carnieGigViews {
 			echo "(tentative)";
 		
 		}       
+		print "\t</td>";
+		print "\t<td>";
+		print "\t\t<a href=\"" . carnieUtil::get_url() . "/ical.php?id=" . $gig[id] . 
+			"\" img src=\"" .  carnieUtil::get_url() . "/images/calendar.jpg\"></a>"
 		print "\t</td>";
 		print "</tr>";
 	}
