67558740503f719aeba87bd932c70a61c665561d
Me <me@carnivalband.nfshost.com>
2010-02-23 04:57:18 +0000

    Nonce verification and submit button.

--- csv.php
+++ csv.php
@@ -13,15 +13,19 @@ $Filename = "CarnieGigs.csv";
 header("Content-Type: text/csv");
 header("Content-Disposition: attachment; filename=$Filename");
 
+// Verify nonce.
+if ( wp_verify_nonce($_POST['carnie-gigs-csv-verify-key'], 'carnie-gigs') ) {
+	$table_name = $wpdb->prefix . "carniegigs";
 
-$table_name = $wpdb->prefix . "carniegigs";
+	$select = "SELECT * FROM " . $table_name;
+	if (! current_user_can('read_private_pages') {
+		$select = "SELECT id date title description location url calltime eventstart performancestart coordinator costume  FROM " . $table_name;
+	}
 
-$select = "SELECT * FROM " . $table_name;
-if (! current_user_can('read_private_pages') {
-	$select = "SELECT id date title description location url calltime eventstart performancestart coordinator costume  FROM " . $table_name;
+	$gigs = $wpdb->get_results( $select, ARRAY_A );
+	carnieGigsCsv($gigs);
+} else {
+	echo '"security failure", "nonce"';
 }
-
-$gigs = $wpdb->get_results( $select, ARRAY_A );
-carnieGigsCsv($gigs);
 
 ?>
