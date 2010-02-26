008aea93f3c43829d6efaf02ba76af31e6f3dc72
Me <me@carnivalband.nfshost.com>
2010-02-26 04:30:17 +0000

    Actually try to delete a gig.

--- controllers/edit-carnie-gigs.php
+++ controllers/edit-carnie-gigs.php
@@ -5,39 +5,20 @@
  */
 class carnieGigEditController {
 
-	private $gigsView ;
+	private $gigsView, $message;
 	/*
 	 * Constructor
 	 */
 	function __construct() {
 		$this->gigsView = new carnieGigViews;
+		$this->message = null;
 	}
 	   
+	   
 	/*
-	 * edit gigs page
+	 * render gigs page
 	 */
-	function edit_gigs_page() {
-		// massage POST data
-		$folder = carnieUtil::get_url();
-
-		if ( get_magic_quotes_gpc() ) {
-			$_POST      = array_map( 'stripslashes_deep', $_POST );
-			$_GET       = array_map( 'stripslashes_deep', $_GET );
-			$_COOKIE    = array_map( 'stripslashes_deep', $_COOKIE );
-			$_REQUEST   = array_map( 'stripslashes_deep', $_REQUEST );
-		}
-
-		$message = null;
-		if ($_POST['method']) {
-			// Verify nonce.
-			if ( wp_verify_nonce($_POST['carnie-gigs-csv-verify-key'], 'carnie-gigs') ) {
-				$message = $_POST['method'];
-			} else {
-				$message = '"security failure", "nonce"';
-			}
-		}
-
-		// Render list
+	function render_gigs_page() {
 		print '<div class="wrap">';
 		echo "<h2>Edit Carnie Gigs";
 
@@ -59,6 +40,55 @@ class carnieGigEditController {
 
 		$this->gigsView->shortGigs($results);
 		print "</div>";
+	}
+
+	/*
+	 * delete a gig
+	 */
+	function delete($gigid) {
+		global $wpdb;
+		$table_name = $wpdb->prefix . "carniegigs";
+
+		$title = $wpdb->get_var($wpdb->prepare("SELECT title FROM $table_name WHERE id = %d;", $gigid));
+
+
+		if ($wpdb->query($wpdb->prepare("DELETE FROM $table_name WHERE id = %d;", $gigid)) {
+			$this->message = "deleted " . $title;
+		} else {
+			$this->message = "Error deleting " . $gigid;
+			$wpdb->print_error();
+		}
+
+	}
+
+	/*
+	 * edit gigs page
+	 */
+	function edit_gigs_page() {
+		// massage POST data
+		$folder = carnieUtil::get_url();
+
+		if ( get_magic_quotes_gpc() ) {
+			$_POST      = array_map( 'stripslashes_deep', $_POST );
+			$_GET       = array_map( 'stripslashes_deep', $_GET );
+			$_COOKIE    = array_map( 'stripslashes_deep', $_COOKIE );
+			$_REQUEST   = array_map( 'stripslashes_deep', $_REQUEST );
+		}
+
+		if ($_POST['method']) {
+			// Verify nonce.
+			if ( wp_verify_nonce($_POST['carnie-gigs-csv-verify-key'], 'carnie-gigs') ) {
+				if ($_POST['method'] == 'delete' && $_POST['gigid']) {
+					$this->delete($_POST['gigid']);
+				}
+
+			} else {
+				$this->message = '"security failure", "nonce"';
+			}
+		}
+
+		// Render list
+		$this->render_gigs_page();
 	}
 }
 ?>
