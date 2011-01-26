ee347034fa6bdef01859f6db618585fcdebb3644
Me <me@carnivalband.nfshost.com>
2011-01-26 04:17:07 +0000

    Give a mouse a cookie, and he will ask for an attendance sheet.

---
+++ csv_attendance.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This php is called directly with GET to get carnie gigs attendance as a spreadsheet.
+ */
+require_once 'ajaxSetup.php';
+require_once 'version.php';
+require_once 'views/attendance_csv.php';
+
+global $wpdb;
+
+$Filename = "CarnieGigsAttendance.csv";
+header("Content-Type: text/csv");
+header("Content-Disposition: attachment; filename=$Filename");
+
+$table_name = get_option('carniegigs_mirror_table');
+$select = "SELECT * FROM " . $table_name;
+
+$gigs = $wpdb->get_results( $select, ARRAY_A );
+carnieGigsAttendanceCsv($gigs);
+
+?>
---
+++ views/attendance_csv.php
@@ -0,0 +1,39 @@
+<?php
+
+function sanitizeCsvField($field) {
+	if ($field != NULL) {
+		// escape " character in field
+		$field = str_replace("\"", "\"\"", $field);
+		// strip newlines in field
+		$field = str_replace(array('\n', '\r'), " ", $field);
+
+	}
+	$field = "\"" . stripslashes($field) . "\"";
+
+	return $field;
+}
+
+function carnieGigsCsvAttendance($gigs) {
+
+	// Date, Gig Name, Carnies, Others
+	echo "\"date\", \"title\", ";
+
+	echo "Others\n";
+	
+	// Data
+	foreach ($gigs as $gig) {
+		// Date
+		echo sanitizeCsvField($gig['date']) . ", ";
+
+		// Title
+		echo sanitizeCsvField($gig['title']) . ", ";
+		
+		// Carnies
+		
+		// Others
+		echo sanitizeCsvField($gig['attendees']);
+
+		echo "\n";
+	}
+}
+?>
