a5292ec540c4becf5301144bfc699c93977d3c4e
Me <me@carnivalband.nfshost.com>
2011-01-27 05:48:58 +0000

    Verify nonce

--- attendance_csv.php
+++ attendance_csv.php
@@ -13,9 +13,14 @@ $Filename = "CarnieGigsAttendance.csv";
 header("Content-Type: text/csv");
 header("Content-Disposition: attachment; filename=$Filename");
 
-$table_name = get_option('carniegigs_mirror_table');
-$select = "SELECT * FROM " . $table_name;
+// Verify nonce.
+if ( wp_verify_nonce($_POST['carnie-gigs-attendance-csv-verify-key'], 'carnie-gigs-attendance') ) {
+	$table_name = get_option('carniegigs_mirror_table');
+	$select = "SELECT * FROM " . $table_name;
 
-$gigs = $wpdb->get_results( $select, ARRAY_A );
-carnieGigsCsvAttendance($gigs);
+	$gigs = $wpdb->get_results( $select, ARRAY_A );
+	carnieGigsCsvAttendance($gigs);
+} else {
+	echo '"security failure", "nonce"';
+}
 ?>
