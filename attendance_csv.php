a76ba09cc1434e139d67d531b5cae14aab2dfde0
Darryl Fuller <darrylf@apple.com>
2012-11-17 20:58:02 -0800

    Limit carnie gigs attendance export to last year and a half

--- attendance_csv.php
+++ attendance_csv.php
@@ -16,7 +16,7 @@ header("Content-Disposition: attachment; filename=$Filename");
 // Verify nonce.
 if ( wp_verify_nonce($_POST['carnie-gigs-attendance-csv-verify-key'], 'carnie-gigs-attendance') ) {
 	$table_name = get_option('carniegigs_mirror_table');
-	$select = "SELECT * FROM " . $table_name;
+	$select = "SELECT * FROM " . $table_name . " WHERE DATEDIFF( CURDATE( ) ,  `date` ) < 547";
 
 	$gigs = $wpdb->get_results( $select, ARRAY_A );
 	carnieGigsCsvAttendance($gigs);
