1e7d6a94c0364510fa1ca5f29a9dfdda9cd59df1
Darryl F <darryl@tzap.ca>
2021-12-23 11:30:14 -0800

    Remove deprecated function from ajax php

--- ajaxSetup.php
+++ ajaxSetup.php
@@ -14,12 +14,5 @@ if ( !defined('WP_CONTENT_DIR') ) define( 'WP_CONTENT_DIR', ABSPATH . 'wp-conten
 
 
 $folder = carnieUtil::get_url();
-
-if ( get_magic_quotes_gpc() ) {
-	$_POST      = array_map( 'stripslashes_deep', $_POST );
-	$_GET       = array_map( 'stripslashes_deep', $_GET );
-	$_COOKIE    = array_map( 'stripslashes_deep', $_COOKIE );
-	$_REQUEST   = array_map( 'stripslashes_deep', $_REQUEST );
-}
 		
 ?>
